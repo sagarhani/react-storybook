1258c7260180f54f7b6252ec23f29d89100e2429
sagarhani <sagarhani33@gmail.com>
2019-04-09 23:14:27 +0530

    Add another story

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
-storiesOf('Button', module).add('with background', () => (
+storiesOf('Button', module)
+.add('with background', () => (
     <Button bg="pink">Hello World</Button>
+))
+.add('with another background', () => (
+    <Button bg="yellow">Hello Another World</Button>
 ));
