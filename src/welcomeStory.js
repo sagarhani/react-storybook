2fac950da96cd1aabfde7a8519524045ce94483e
sagarhani <sagarhani33@gmail.com>
2019-04-09 23:21:12 +0530

    Welcome Screen to Storybook 🤠

---
+++ src/welcomeStory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf }  from '@storybook/react';
+
+storiesOf('Welcome', module).add(
+    'to your storybook 🤠',
+    () => (
+        <h1>Welcome to your Storybook</h1>
+    )
+);
